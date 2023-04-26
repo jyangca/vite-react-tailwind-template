8ac6a04328fe51a77d2397cf8ede081d6cf6e3b3
Jiho YANG <yjsm111@nuua.ai>
2023-04-26 12:20:48 +0900

    CHORE: font family

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
   },
   plugins: [],
-};
+}
